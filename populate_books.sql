e7b894e4ea909734b8fee67d7825264b8f3fff0a
William Saunders <willjsaunders@icloud.com>
2024-08-07 19:28:28 +0000

    feat: Add books seed to db

--- populate_books.sql
+++ populate_books.sql
@@ -1,6 +1,11 @@
--- books_seed.sql
+CREATE TABLE Book (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    author TEXT NOT NULL,
+    published_date DATE
+);
 
-INSERT INTO book (title, author, published_date) VALUES
+INSERT INTO Book (title, author, published_date) VALUES
 ('To Kill a Mockingbird', 'Harper Lee', '1960-07-11'),
 ('1984', 'George Orwell', '1949-06-08'),
 ('Pride and Prejudice', 'Jane Austen', '1813-01-28'),
