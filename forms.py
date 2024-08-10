3e83b2b02351e786ed5b178b0cad44b44c04b055
William Saunders <willjsaunders@icloud.com>
2024-08-10 12:16:25 +0000

    feat: Add remember me functionality to login form

--- forms.py
+++ forms.py
@@ -1,6 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, URLField
 from wtforms.validators import DataRequired, Email, EqualTo, Optional, URL
+from wtforms import BooleanField
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Login')
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
