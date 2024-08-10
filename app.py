72639734e7911d32205b46e154eb6b3996141ffe
William Saunders <willjsaunders@icloud.com>
2024-08-10 10:55:24 +0000

    feat: Generate secret key for Flask app configuration

--- app.py
+++ app.py
@@ -2,12 +2,17 @@ from flask import Flask, render_template, request # type: ignore
 import os
 from extensions import db, migrate
 from models import Book, Review, Category, BookCategory, ReviewVote, User, Authentication
+from forms import RegistrationForm
+import secrets
 
 app = Flask(__name__)
 
 # Configuration for SQLite database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.instance_path, 'books.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Generate a secret key
+secret_key = '+$2At1z~QE7_^7il`'
+app.config['SECRET_KEY'] = secret_key
 
 # Initialize the database
 db.init_app(app)
