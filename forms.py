796a052dd330f2481a784e4fc510afa12015b0a6
William Saunders <willjsaunders@icloud.com>
2024-08-10 11:01:42 +0000

    feat: Add profile picture URL field to registration form

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, URLField
 from wtforms.validators import DataRequired, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
@@ -7,4 +7,5 @@ class RegistrationForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    profile_picture_url = URLField('Profile Picture URL', validators=[Optional(), URL()])
     submit = SubmitField('Register')
